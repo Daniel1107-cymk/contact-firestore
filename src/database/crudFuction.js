daa30cc00e97bc47b023e2f2954bc1d119320012
Daniel Lim <daniellim4400@gmail.com>
2025-07-06 17:26:15 +0700

    feat: update crudFunction to use @react-native-firebase/firestore

--- src/database/crudFuction.js
+++ src/database/crudFuction.js
@@ -1,58 +1,63 @@
-import { query, getDocs, collection, addDoc, doc, updateDoc, deleteDoc, orderBy, where, setDoc, getDoc } from "firebase/firestore";
-import Db from "./firebase";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+} from "@react-native-firebase/firestore";
+import { Db } from "./firebase";
 
 const CrudFunction = {
-    fetchDataCol: async (col) => {
-        const q = query(
-            collection(Db, col),
-            orderBy('createdAt', 'desc')
-        );
+  fetchDataCol: async (col) => {
+    const q = query(collection(Db, col), orderBy("createdAt", "desc"));
 
-        const snapshot = await getDocs(q);
-        const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    const snapshot = await getDocs(q);
+    const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 
-        return data
-    },
+    return data;
+  },
 
-    fetchDataDoc: async (col, doc) => {
-        const docRef = doc(Db, col, doc);
-        const docSnap = await getDoc(docRef);
+  fetchDataDoc: async (col, doc) => {
+    const docRef = doc(Db, col, doc);
+    const docSnap = await getDoc(docRef);
 
-        if (docSnap.exists()) {
-            const userData = docSnap.data()
-            return userData
-        } else {
-            console.log('No such document!');
-            return null;
-        }
-    },
-
-    addDataCol: async (col, data) => {
-        const itemRef = collection(Db, col)
-        return await addDoc(itemRef, data);
-    },
-
-    addDataDoc: async (col1, doc1, col2, doc2, data) => {
-        const itemRef = doc(Db, col1, doc1, col2, doc2);
-        return await setDoc(itemRef, data);
-    },
-
-    updateData: async (col, id, data) => {
-        const itemRef = doc(Db, col, id);
-        return await updateDoc(itemRef, data);
-    },
-
-    deleteData: async (col, id) => {
-        const itemRef = doc(Db, col, id);
-        return await deleteDoc(itemRef);
-    },
-
-    search: async (col, field, keyword) => {
-        const q = query(collection(Db, col), where(field, '==', keyword));
-        const snapshot = await getDocs(q);
-        const results = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        return results;
+    if (docSnap.exists()) {
+      const userData = docSnap.data();
+      return userData;
+    } else {
+      console.log("No such document!");
+      return null;
     }
-}
+  },
 
-export default CrudFunction
+  addDataCol: async (col, data) => {
+    const itemRef = collection(Db, col);
+    return await addDoc(itemRef, data);
+  },
+
+  addDataDoc: async (col1, doc1, col2, doc2, data) => {
+    const itemRef = doc(Db, col1, doc1, col2, doc2);
+    return await setDoc(itemRef, data);
+  },
+
+  updateData: async (col, id, data) => {
+    const itemRef = doc(Db, col, id);
+    return await updateDoc(itemRef, data);
+  },
+
+  deleteData: async (col, id) => {
+    const itemRef = doc(Db, col, id);
+    return await deleteDoc(itemRef);
+  },
+
+  search: async (col, field, keyword) => {
+    const q = query(collection(Db, col), where(field, "==", keyword));
+    const snapshot = await getDocs(q);
+    const results = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    return results;
+  },
+};
+
+export default CrudFunction;
